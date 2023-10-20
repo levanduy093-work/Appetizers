7e3b6c8580bc530eab7f63034982bd8604cc3b43
levanduy093-work <levanduy.work@gmail.com>
2023-10-20 17:15:04 +0700

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Lê Văn Duy on 20/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
